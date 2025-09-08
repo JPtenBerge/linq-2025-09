1df373171450718e13b0b7852389b3d0cbc3c2c0
JP <jp@jp.nl>
2025-09-08 09:41:00 +0200

    Eerste demo: lambdas

---
+++ demo/DemoSolution/LambdaDemo/ProductShower.cs
@@ -0,0 +1,50 @@
+﻿namespace LambdaDemo;
+
+public class ProductShower
+{
+	public static List<Product> Products { get; set; } = new()
+	{
+		new() { Description = "Muis", Price = 4.99M },
+		new() { Description = "Toetsenbord", Price = 14.80M },
+		new() { Description = "Scherm", Price = 299.99M },
+		new() { Description = "Desinfectiespul", Price = 1.50M },
+		new() { Description = "Camera", Price = 140M },
+		new() { Description = "Meeting Owl Pro", Price = 1400M },
+		new() { Description = "Laptop", Price = 2700M },
+		new() { Description = "Markeerstift", Price = 0.50M },
+	};
+
+	public static void FilterProducts(Predicate<Product> filter, Action<Product> printer)
+	{
+		foreach (var product in Products)
+		{
+			if (filter.Invoke(product))
+			{
+				// Console.WriteLine($"{product.Description} kost {product.Price}");
+				printer.Invoke(product);
+			}
+		}
+	}
+
+	public static void FilterDureProducten()
+	{
+		foreach (var product in Products)
+		{
+			if (product.Price > 100M)
+			{
+				Console.WriteLine($"{product.Description} is duur want kost {product.Price}");
+			}
+		}
+	}
+	
+	public static void FilterProductenBeginnendMet(string letter)
+	{
+		foreach (var product in Products)
+		{
+			if (product.Description.StartsWith(letter))
+			{
+				Console.WriteLine($"{product.Description} begint met {letter}");
+			}
+		}
+	}
+}
---
+++ demo/DemoSolution/LambdaDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LambdaDemo;
+
+ProductShower.FilterDureProducten();
+ProductShower.FilterProductenBeginnendMet("M");
+
+
+// 3 delegates:
+// - Action<T> - geen returnwaarde
+// - Predicate<T> - altijd een boolean als returnwaarde
+// - Func<T, bool> - instelbare returnwaarde
+
+Console.WriteLine("==============");
+Predicate<Product> duurPred = delegate(Product p) { return p.Price > 50; };
+Predicate<Product> duurPred2 = p => p.Price > 50;
+
+// ProductShower.FilterProducts(duurPred);
+// ProductShower.FilterProducts(duurPred2);
+ProductShower.FilterProducts(p => p.Price > 50, p => Console.WriteLine($"Product hier is duur: {p.Description}"));
+
+Parallel.Invoke(
+	() => Console.WriteLine("bla"),
+	() => Console.WriteLine("bla"),
+	() => Console.WriteLine("bla"),
+	() => Console.WriteLine("bla"),
+	() => Console.WriteLine("bla")
+);
+
